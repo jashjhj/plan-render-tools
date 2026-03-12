c90fea765f020e0b09f112841e5b5bc72a9e29a7
jashjhj <jasmineeades7@gmail.com>
2026-03-12 03:48:23 +0000

    Added targetting reticule

--- main.cpp
+++ main.cpp
@@ -2,6 +2,9 @@
 #include <stdio.h>
 #include <string.h>
 
+#define HEIGHT 1000
+#define WIDTH 1000
+
 sf::Image image;
 int image_size_x = 1;
 int image_size_y = 1;
@@ -21,39 +24,46 @@ int load_image(char* filepath){
     return 0;
 }
 
+void set_sprite_scale(sf::Sprite* sprite){
+    float h_ratio = (float)WIDTH/(float)image_size_x;
+    float w_ratio = (float)HEIGHT/(float)image_size_y;
+    float ratio = h_ratio < w_ratio ? h_ratio : w_ratio; // picks smaller ratio of the two
+    sprite->setScale(sf::Vector2f(ratio, ratio));
+}
 
 
 
 int main(int argc, char** argv)
 {
-    sf::RenderWindow window(sf::VideoMode(1000, 1000), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), "SFML works!");
 
     sf::Texture texture;
     if (argc == 2){
         load_image(argv[1]);
     }
 
-    while (true)
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
+    int target_x = 0;
+    int target_y = 0;
+    sf::CircleShape target(4.0f);
+    target.setFillColor(sf::Color::Magenta);
+    target.setOrigin(sf::Vector2f(2.0f, 2.0f));
+
+    while (true){
+
 
         sf::Texture texture;
         texture.loadFromImage(image);
         sf::Sprite sprite;
         sprite.setTexture(texture);
+        set_sprite_scale(&sprite);
 
         window.clear();
         window.draw(sprite);
-        //window.draw(shape);
+        window.draw(target);
         window.display();
 
+        //////////////////////////////////////  PARSE INPUT  /////////////////////////////////
+        //////////////////////////////////////////////////////////////////////////////////////
 
         char input[128]; // parse input
         char operation[128];
@@ -84,12 +94,30 @@ int main(int argc, char** argv)
             window.close();
             return 0;
         }else if(!strcmp(operation, "help")){
-            printf("help\nexit\n");
+            printf("help\nexit\n\
+            sx    Set target x\
+            sy    Set target y\
+            nx    Nudge target x\
+            ny    Nudge target y\
+            ");
 
         }else if(!strcmp(operation, "load")){
             load_image(operand);
         }else if(!strcmp(operation, "redraw")){
-            
+        
+        }else if(!strcmp(operation, "sx")){
+            target_x = atoi(operand);
+            target.setPosition(sf::Vector2f(target_x, target_y));
+        }else if(!strcmp(operation, "sy")){
+            target_y = atoi(operand);
+            target.setPosition(sf::Vector2f(target_x, target_y));
+        }else if(!strcmp(operation, "nx")){
+            target_x += atoi(operand);
+            target.setPosition(sf::Vector2f(target_x, target_y));
+        }else if(!strcmp(operation, "ny")){
+            target_y += atoi(operand);
+            target.setPosition(sf::Vector2f(target_x, target_y));
+        
         }else{
             printf("Invalid command: %s\n", operation);
             printf("Write help for a list of commands.\n");
